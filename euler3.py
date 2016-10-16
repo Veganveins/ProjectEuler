c49ba32ec1a9f3c2b812d474d58c28aab58278ac
Jane Thompson <jane@cognius.com>
2016-10-15 23:28:43 -0400

    Largest Prime Factor of 600851475143

---
+++ euler3.py
@@ -0,0 +1,28 @@
+#What is the largest prime factor of the number 600851475143
+
+def is_factor(n,m):
+    if n%m == 0:
+        return True
+    else:
+        return False
+
+def is_Prime(n):
+    for i in range(2,n):
+        if n%i == 0:
+            return False
+    return True
+
+def largest_prime(n):
+    ceiling = n
+    largest = 1
+    i = 2
+    while i < ceiling:
+        if n%i == 0 and is_Prime(i):
+            largest = i
+            ceiling = n/i
+            i += 1
+        i += 1
+        
+    return largest, i
+
+print largest_prime(600851475143)
