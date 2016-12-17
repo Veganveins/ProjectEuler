ecdfa7b8a1587d0c7372e9def00324a46aefac85
Jane Thompson <jane@cognius.com>
2016-12-17 00:19:31 -0500

    Distinct Powers

---
+++ euler29.py
@@ -0,0 +1,10 @@
+def distinct_powers(a,b):
+    powers = []
+    for i in range (2,a+1):
+        for j in range(2,b+1):
+            if i**j not in powers:
+                powers.append(i**j)
+    return len(powers)
+    
+
+print distinct_powers(100,100)
